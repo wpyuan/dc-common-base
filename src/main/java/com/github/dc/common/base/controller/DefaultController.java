0ec7e2b9e0aca44996479af2a6ab6d1064e3546a
wangpeiyuan <490176245@qq.com>
2022-03-24 15:06:43 +0800

    添加 接口swagger注解

--- src/main/java/com/github/dc/common/base/controller/DefaultController.java
+++ src/main/java/com/github/dc/common/base/controller/DefaultController.java
@@ -1,6 +1,7 @@
 package com.github.dc.common.base.controller;
 
 import com.github.dc.common.base.service.DefaultService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,47 +16,55 @@ public class DefaultController<E> {
     @Autowired
     private DefaultService<E> service;
 
+    @ApiOperation("新建")
     @PostMapping("/create")
     public ResponseEntity<E> create(@RequestBody E entity) {
         service.insertSelective(entity);
         return ResponseEntity.ok(entity);
     }
 
+    @ApiOperation("新建")
     @PostMapping("/create-form")
     public ResponseEntity<E> createByForm(E entity) {
         service.insertSelective(entity);
         return ResponseEntity.ok(entity);
     }
 
+    @ApiOperation("删除")
     @DeleteMapping("/remove")
     public ResponseEntity<?> remove(E entity) {
         service.delete(entity);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation("修改")
     @PutMapping("/update-form")
     public ResponseEntity<E> updateByForm(E entity) {
         service.updateByPrimaryKey(entity);
         return ResponseEntity.ok(entity);
     }
 
+    @ApiOperation("修改")
     @PutMapping("/update")
     public ResponseEntity<E> update(@RequestBody E entity) {
         service.updateByPrimaryKey(entity);
         return ResponseEntity.ok(entity);
     }
 
+    @ApiOperation("保存")
     @PostMapping("/save")
     public ResponseEntity<E> save(@RequestBody E entity) {
         service.save(entity);
         return ResponseEntity.ok(entity);
     }
 
+    @ApiOperation("详情")
     @GetMapping("/detail")
     public ResponseEntity<E> detail(E entity) {
         return ResponseEntity.ok(service.detail(entity));
     }
 
+    @ApiOperation("列表")
     @GetMapping("/list")
     public ResponseEntity<List<E>> list(E entity) {
         return ResponseEntity.ok(service.list(entity));
